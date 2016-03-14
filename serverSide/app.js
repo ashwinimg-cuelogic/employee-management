11bcd5eaf17aa52f67d760435b0f448f0f7e2f52
ashwinimg-cuelogic <ashwini.gawade@cuelogic.co.in>
2016-03-14 11:48:50 +0530

    #comment installed depedencies and started the node server on port taking from .env file and registered the hapi-router plugin

---
+++ serverSide/app.js
@@ -0,0 +1,54 @@
+var hapi = require("hapi");
+var Path = require('path');
+require('dotenv').load();
+
+var server = new hapi.Server();
+
+var web = server.connection({
+    port: process.env.SERVER_PORT,
+    routes : {
+        validate:{
+            options:{
+                abortEarly:false
+            }
+        },
+        files : {
+            relativeTo : Path.join(__dirname, 'public/')
+        },
+        cors : true
+    }
+});
+
+
+//register the plugins and stat the server in callback
+server.register([
+    {
+        register : require('hapi-router'),
+        options : {
+            routes : 'src/routes/*.js'
+        }
+    },
+    {
+        register: require('good'),
+        options: {
+            reporters: [{
+                reporter: require('good-console'),
+                events: {
+                    response: '*',
+                    log: '*'
+                }
+            }]
+        }
+
+    }], function (err) {
+    if (err) {
+        throw err;
+    }
+    server.start(function(){
+        server.log("server is running at"+ server.info.uri);
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
+
+module.exports = server;
+
