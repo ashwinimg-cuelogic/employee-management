7cddea67299e1c99146bcb3bf508b36c44e5edc6
ashwinimg-cuelogic <ashwini.gawade@cuelogic.co.in>
2016-03-15 18:34:57 +0530

    #comment added restangular configuration

--- clientSide/scripts/app.js
+++ clientSide/scripts/app.js
@@ -1,1 +1,15 @@
-angular.module('app', ['ui.router'])
+angular.module('app', ['ui.router', 'restangular'])
+
+//setting data for restangular
+.config(["RestangularProvider", function(RestangularProvider){
+    RestangularProvider.setRestangularFields({
+        id: "_id"
+    });
+    //  set the base url for restangular api calls
+    RestangularProvider.setBaseUrl("http://localhost:3000/")
+
+    RestangularProvider.setResponseExtractor(function(response, operation) {
+        return response;
+    });
+
+}])
